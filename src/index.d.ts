2a13f00b8d556247242848061dee755abf2a84ff
Mark Lee <electronjs@lazymalevolence.com>
2020-02-18 13:56:08 -0800

    Standardize whitespace

--- src/index.d.ts
+++ src/index.d.ts
@@ -29,184 +29,184 @@ export = electronPackager;
 declare function electronPackager(opts: electronPackager.Options): Promise<string[]>;
 
 declare namespace electronPackager {
-    type IgnoreFunction = (path: string) => boolean;
-    type HookFunction = (
-        buildPath: string,
-        electronVersion: string,
-        platform: string,
-        arch: string,
-        callbackFn: () => void
-    ) => void;
-    type arch = 'ia32' | 'x64' | 'armv7l' | 'arm64' | 'mips64el' | 'all';
-    type platform = 'linux' | 'win32' | 'darwin' | 'mas' | 'all';
+  type IgnoreFunction = (path: string) => boolean;
+  type HookFunction = (
+    buildPath: string,
+    electronVersion: string,
+    platform: string,
+    arch: string,
+    callbackFn: () => void
+  ) => void;
+  type arch = 'ia32' | 'x64' | 'armv7l' | 'arm64' | 'mips64el' | 'all';
+  type platform = 'linux' | 'win32' | 'darwin' | 'mas' | 'all';
 
-    interface AsarOptions {
-        ordering?: string;
-        unpack?: string;
-        unpackDir?: string;
-    }
+  interface AsarOptions {
+    ordering?: string;
+    unpack?: string;
+    unpackDir?: string;
+  }
 
-    interface ElectronOsxSignOptions
-        extends Omit<SignOptions, 'app' | 'binaries' | 'identity' | 'platform' | 'version'> {
-        identity?: string | true;
-    }
+  interface ElectronOsxSignOptions
+  extends Omit<SignOptions, 'app' | 'binaries' | 'identity' | 'platform' | 'version'> {
+    identity?: string | true;
+  }
 
-    type ElectronNotarizeOptions = Omit<NotarizeOptions, 'appBundleId' | 'appPath'>;
+  type ElectronNotarizeOptions = Omit<NotarizeOptions, 'appBundleId' | 'appPath'>;
+
+  /**
+   * Object (also known as a "hash") of application metadata to embed into the executable
+   */
+  interface Win32Metadata {
+    CompanyName?: string;
+    FileDescription?: string;
+    OriginalFilename?: string;
+    ProductName?: string;
+    InternalName?: string;
+    'requested-execution-level'?: 'asInvoker' | 'highestAvailable' | 'requireAdministrator';
+    'application-manifest'?: string;
+  }
+
+  /** Electron-packager Options. */
+  interface Options {
+    /** The source directory. */
+    dir: string;
+    /**
+     * Optional list of methods to call on completion of each process
+     */
+    afterCopy?: HookFunction[];
+    afterExtract?: HookFunction[];
+    afterPrune?: HookFunction[];
+    /** Shortcut for `--arch=all --platform=all`. */
+    all?: boolean;
+    /**
+     * The human-readable copyright line for the app. Maps to the LegalCopyright metadata property on Windows, and NSHumanReadableCopyright on OS X.
+     */
+    appCopyright?: string;
+    /**
+     * The release version of the application. By default the version property in the package.json is used but it can be overridden with this argument.
+     * If neither are provided, the version of Electron will be used. Maps to the ProductVersion metadata property on Windows, and CFBundleShortVersionString on OS X.
+     */
+    appVersion?: string;
+    /**
+     * The target system architecture(s) to build for. Not required if the all option is set.
+     * If arch is set to all, all supported architectures for the target platforms specified by platform will be built.
+     * Arbitrary combinations of individual architectures are also supported via a comma-delimited string or array of strings.
+     * The non-all values correspond to the architecture names used by Electron releases. This value is not restricted to the official set if download.mirror is set.
+     */
+    arch?: arch | arch[];
+    /**
+     * Whether to package the application's source code into an archive, using Electron's archive format
+     */
+    asar?: boolean | AsarOptions;
+    /**
+     * The path to a prebuilt ASAR file.
+     */
+    prebuiltAsar?: string;
+    /**
+     * The build version of the application. Defaults to the value of appVersion.
+     * Maps to the FileVersion metadata property on Windows, and CFBundleVersion on OS X.
+     */
+    buildVersion?: string;
+    /**
+     * Whether symlinks should be dereferenced during the copying of the application source.
+     */
+    derefSymlinks?: boolean;
+    /**
+     * If present, passes custom options to `@electron/get`
+     */
+    download?: ElectronDownloadOptions;
+    /**
+     * The Electron version with which the app is built (without the leading 'v') - for example, 1.4.13
+     */
+    electronVersion?: string;
+    /**
+     * One or more files to be copied directly into the app's Contents/Resources directory for OS X target platforms, and the resources directory for other target platforms.
+     */
+    extraResource?: string | string[];
+    /**
+     * The name of the executable file, sans file extension. Defaults to the value for the name parameter
+     */
+    executableName?: string;
+    /**
+     * The local path to the icon file, if the target platform supports setting embedding an icon.
+     */
+    icon?: string;
+    /**
+     * One or more additional regular expression patterns which specify which files to ignore when copying files to create the app bundle(s).
+     * The regular expressions are matched against the absolute path of a given file/directory to be copied.
+     */
+    ignore?: RegExp | RegExp[] | IgnoreFunction;
+    /**
+     * The application name. If omitted, it will use the productName or name value from the nearest package.json
+     */
+    name?: string;
+    /** The output directory. */
+    out?: string;
+    /**
+     * Whether to replace an already existing output directory for a given platform (true) or skip recreating it (false).
+     */
+    overwrite?: boolean;
+    /**
+     * The target platform(s) to build for. Not required if the all option is set.
+     */
+    platform?: platform;
+    /**
+     * Runs the package manager command to remove all of the packages specified in the devDependencies section of package.json from the outputted Electron app.
+     */
+    prune?: boolean;
+    /**
+     * If true, disables printing informational and warning messages to the console when packaging the application. This does not disable errors.
+     */
+    quiet?: boolean;
+    /**
+     * The base directory to use as a temp directory. Set to false to disable use of a temporary directory.
+     */
+    tmpdir?: string | false;
 
     /**
-     * Object (also known as a "hash") of application metadata to embed into the executable
+     * OS X/Mac App Store targets only
      */
-    interface Win32Metadata {
-        CompanyName?: string;
-        FileDescription?: string;
-        OriginalFilename?: string;
-        ProductName?: string;
-        InternalName?: string;
-        'requested-execution-level'?: 'asInvoker' | 'highestAvailable' | 'requireAdministrator';
-        'application-manifest'?: string;
-    }
 
-    /** Electron-packager Options. */
-    interface Options {
-        /** The source directory. */
-        dir: string;
-        /**
-         * Optional list of methods to call on completion of each process
-         */
-        afterCopy?: HookFunction[];
-        afterExtract?: HookFunction[];
-        afterPrune?: HookFunction[];
-        /** Shortcut for `--arch=all --platform=all`. */
-        all?: boolean;
-        /**
-         * The human-readable copyright line for the app. Maps to the LegalCopyright metadata property on Windows, and NSHumanReadableCopyright on OS X.
-         */
-        appCopyright?: string;
-        /**
-         * The release version of the application. By default the version property in the package.json is used but it can be overridden with this argument.
-         * If neither are provided, the version of Electron will be used. Maps to the ProductVersion metadata property on Windows, and CFBundleShortVersionString on OS X.
-         */
-        appVersion?: string;
-        /**
-         * The target system architecture(s) to build for. Not required if the all option is set.
-         * If arch is set to all, all supported architectures for the target platforms specified by platform will be built.
-         * Arbitrary combinations of individual architectures are also supported via a comma-delimited string or array of strings.
-         * The non-all values correspond to the architecture names used by Electron releases. This value is not restricted to the official set if download.mirror is set.
-         */
-        arch?: arch | arch[];
-        /**
-         * Whether to package the application's source code into an archive, using Electron's archive format
-         */
-        asar?: boolean | AsarOptions;
-        /**
-         * The path to a prebuilt ASAR file.
-         */
-        prebuiltAsar?: string;
-        /**
-         * The build version of the application. Defaults to the value of appVersion.
-         * Maps to the FileVersion metadata property on Windows, and CFBundleVersion on OS X.
-         */
-        buildVersion?: string;
-        /**
-         * Whether symlinks should be dereferenced during the copying of the application source.
-         */
-        derefSymlinks?: boolean;
-        /**
-         * If present, passes custom options to `@electron/get`
-         */
-        download?: ElectronDownloadOptions;
-        /**
-         * The Electron version with which the app is built (without the leading 'v') - for example, 1.4.13
-         */
-        electronVersion?: string;
-        /**
-         * One or more files to be copied directly into the app's Contents/Resources directory for OS X target platforms, and the resources directory for other target platforms.
-         */
-        extraResource?: string | string[];
-        /**
-         * The name of the executable file, sans file extension. Defaults to the value for the name parameter
-         */
-        executableName?: string;
-        /**
-         * The local path to the icon file, if the target platform supports setting embedding an icon.
-         */
-        icon?: string;
-        /**
-         * One or more additional regular expression patterns which specify which files to ignore when copying files to create the app bundle(s).
-         * The regular expressions are matched against the absolute path of a given file/directory to be copied.
-         */
-        ignore?: RegExp | RegExp[] | IgnoreFunction;
-        /**
-         * The application name. If omitted, it will use the productName or name value from the nearest package.json
-         */
-        name?: string;
-        /** The output directory. */
-        out?: string;
-        /**
-         * Whether to replace an already existing output directory for a given platform (true) or skip recreating it (false).
-         */
-        overwrite?: boolean;
-        /**
-         * The target platform(s) to build for. Not required if the all option is set.
-         */
-        platform?: platform;
-        /**
-         * Runs the package manager command to remove all of the packages specified in the devDependencies section of package.json from the outputted Electron app.
-         */
-        prune?: boolean;
-        /**
-         * If true, disables printing informational and warning messages to the console when packaging the application. This does not disable errors.
-         */
-        quiet?: boolean;
-        /**
-         * The base directory to use as a temp directory. Set to false to disable use of a temporary directory.
-         */
-        tmpdir?: string | false;
+    /**
+     * The bundle identifier to use in the application's plist.
+     */
+    appBundleId?: string;
+    /**
+     * The application category type, as shown in the Finder via View → Arrange by Application Category when viewing the Applications directory.
+     */
+    appCategoryType?: string;
+    /**
+     * When the value is a String, the filename of a plist file. Its contents are added to the app's plist.
+     * When the value is an Object, an already-parsed plist data structure that is merged into the app's plist.
+     */
+    extendInfo?: string | { [property: string]: any };
+    /**
+     * The bundle identifier to use in the application helper's plist.
+     */
+    helperBundleId?: string;
+    /**
+     * Forces support for Mojave (macOS 10.14) dark mode in the packaged app.
+     */
+    darwinDarkModeSupport?: boolean;
+    /**
+     * If present, notarizes OS X target apps when the host platform is OS X and XCode is installed.
+     */
+    osxNotarize?: ElectronNotarizeOptions;
+    /**
+     * If present, signs OS X target apps when the host platform is OS X and XCode is installed.
+     */
+    osxSign?: boolean | ElectronOsxSignOptions;
 
-        /**
-         * OS X/Mac App Store targets only
-         */
+    /** The URL protocol schemes the app supports. */
+    protocols?: Array<{
+      name: string;
+      schemes: string[];
+    }>;
 
-        /**
-         * The bundle identifier to use in the application's plist.
-         */
-        appBundleId?: string;
-        /**
-         * The application category type, as shown in the Finder via View → Arrange by Application Category when viewing the Applications directory.
-         */
-        appCategoryType?: string;
-        /**
-         * When the value is a String, the filename of a plist file. Its contents are added to the app's plist.
-         * When the value is an Object, an already-parsed plist data structure that is merged into the app's plist.
-         */
-        extendInfo?: string | { [property: string]: any };
-        /**
-         * The bundle identifier to use in the application helper's plist.
-         */
-        helperBundleId?: string;
-        /**
-         * Forces support for Mojave (macOS 10.14) dark mode in the packaged app.
-         */
-        darwinDarkModeSupport?: boolean;
-        /**
-         * If present, notarizes OS X target apps when the host platform is OS X and XCode is installed.
-         */
-        osxNotarize?: ElectronNotarizeOptions;
-        /**
-         * If present, signs OS X target apps when the host platform is OS X and XCode is installed.
-         */
-        osxSign?: boolean | ElectronOsxSignOptions;
+    /**
+     * Windows targets only
+     */
 
-        /** The URL protocol schemes the app supports. */
-        protocols?: Array<{
-            name: string;
-            schemes: string[];
-        }>;
-
-        /**
-         * Windows targets only
-         */
-
-        win32metadata?: Win32Metadata;
-    }
+    win32metadata?: Win32Metadata;
+  }
 }
--- test/index.test-d.ts
+++ test/index.test-d.ts
@@ -1,17 +1,17 @@
 import packager = require('..');
 
 function completeFunction(
-    buildPath: string,
-    electronVersion: string,
-    platform: string,
-    arch: string,
-    callbackFn: () => void
+  buildPath: string,
+  electronVersion: string,
+  platform: string,
+  arch: string,
+  callbackFn: () => void
 ) {
-    callbackFn();
+  callbackFn();
 }
 
 function ignoreFunction(path: string) {
-    return true;
+  return true;
 }
 
 function onCompleted(appPaths: string[]) {}
@@ -19,194 +19,174 @@ function onCompleted(appPaths: string[]) {}
 function onError(error: Error) {}
 
 packager({
-    dir: '.',
-    name: 'myapplication',
-    platform: 'win32',
-    arch: 'all',
-    electronVersion: '0.34.0',
-    win32metadata: {
-        CompanyName: 'Acme CO',
-        FileDescription: 'My application',
-        OriginalFilename: 'myapp.exe',
-        ProductName: 'Application',
-        InternalName: 'roadrunner',
-        'requested-execution-level': 'highestAvailable',
-        'application-manifest': 'manifest.xml',
-    },
-})
-    .then(onCompleted)
-    .catch(onError);
+  dir: '.',
+  name: 'myapplication',
+  platform: 'win32',
+  arch: 'all',
+  electronVersion: '0.34.0',
+  win32metadata: {
+    CompanyName: 'Acme CO',
+    FileDescription: 'My application',
+    OriginalFilename: 'myapp.exe',
+    ProductName: 'Application',
+    InternalName: 'roadrunner',
+    'requested-execution-level': 'highestAvailable',
+    'application-manifest': 'manifest.xml',
+  },
+}).then(onCompleted).catch(onError);
 
 packager({
-    dir: '.',
-    name: 'myapplication',
-    electronVersion: '0.34.0',
-    all: true,
-    win32metadata: {
-        CompanyName: 'Acme CO',
-        FileDescription: 'My application',
-        OriginalFilename: 'myapp.exe',
-        ProductName: 'Application',
-        InternalName: 'roadrunner',
-        'requested-execution-level': 'requireAdministrator',
-        'application-manifest': 'manifest.xml',
-    },
-})
-    .then(onCompleted)
-    .catch(onError);
+  dir: '.',
+  name: 'myapplication',
+  electronVersion: '0.34.0',
+  all: true,
+  win32metadata: {
+    CompanyName: 'Acme CO',
+    FileDescription: 'My application',
+    OriginalFilename: 'myapp.exe',
+    ProductName: 'Application',
+    InternalName: 'roadrunner',
+    'requested-execution-level': 'requireAdministrator',
+    'application-manifest': 'manifest.xml',
+  },
+}).then(onCompleted).catch(onError);
 
 packager({
-    dir: '.',
-    name: 'myapplication',
-    platform: 'all',
-    arch: 'all',
-    electronVersion: '0.34.0',
-})
-    .then(onCompleted)
-    .catch(onError);
+  dir: '.',
+  name: 'myapplication',
+  platform: 'all',
+  arch: 'all',
+  electronVersion: '0.34.0',
+}).then(onCompleted).catch(onError);
 
 packager({
-    dir: '.',
-    name: 'myapplication',
-    electronVersion: '0.34.0',
-    all: true,
-})
-    .then(onCompleted)
-    .catch(onError);
+  dir: '.',
+  name: 'myapplication',
+  electronVersion: '0.34.0',
+  all: true,
+}).then(onCompleted).catch(onError);
 
 packager({
-    dir: '.',
-    name: 'myapplication',
-    electronVersion: '0.34.0',
-    arch: 'arm64',
-    executableName: 'myapp',
-})
-    .then(onCompleted)
-    .catch(onError);
+  dir: '.',
+  name: 'myapplication',
+  electronVersion: '0.34.0',
+  arch: 'arm64',
+  executableName: 'myapp',
+}).then(onCompleted).catch(onError);
 
 packager({
-    dir: '.',
-    afterCopy: [completeFunction],
-    afterExtract: [completeFunction],
-    afterPrune: [completeFunction],
-    appCopyright: 'Copyright',
-    appVersion: '1.0',
-    arch: 'ia32',
-    asar: false,
-    buildVersion: '1.2.3',
-    derefSymlinks: true,
-    download: {
-        cacheRoot: './zips',
-        mirrorOptions: {
-            mirror: 'https://10.1.2.105/',
-        },
+  dir: '.',
+  afterCopy: [completeFunction],
+  afterExtract: [completeFunction],
+  afterPrune: [completeFunction],
+  appCopyright: 'Copyright',
+  appVersion: '1.0',
+  arch: 'ia32',
+  asar: false,
+  buildVersion: '1.2.3',
+  derefSymlinks: true,
+  download: {
+    cacheRoot: './zips',
+    mirrorOptions: {
+      mirror: 'https://10.1.2.105/',
     },
-    extraResource: 'foo.js',
-    icon: 'foo.ico',
-    ignore: /ab+c/,
-    out: 'out',
-    overwrite: true,
-    prune: true,
-    quiet: true,
-    tmpdir: '/tmp',
-    win32metadata: {
-        CompanyName: 'Acme CO',
-        FileDescription: 'My application',
-        OriginalFilename: 'myapp.exe',
-        ProductName: 'Application',
-        InternalName: 'roadrunner',
-        'requested-execution-level': 'asInvoker',
-        'application-manifest': 'manifest.xml',
-    },
-})
-    .then(onCompleted)
-    .catch(onError);
+  },
+  extraResource: 'foo.js',
+  icon: 'foo.ico',
+  ignore: /ab+c/,
+  out: 'out',
+  overwrite: true,
+  prune: true,
+  quiet: true,
+  tmpdir: '/tmp',
+  win32metadata: {
+    CompanyName: 'Acme CO',
+    FileDescription: 'My application',
+    OriginalFilename: 'myapp.exe',
+    ProductName: 'Application',
+    InternalName: 'roadrunner',
+    'requested-execution-level': 'asInvoker',
+    'application-manifest': 'manifest.xml',
+  },
+}).then(onCompleted).catch(onError);
 
 packager({
-    dir: '.',
-    arch: 'x64',
-    asar: true,
-    derefSymlinks: false,
-    download: {
-        cacheRoot: './zips',
-        mirrorOptions: {
-            mirror: 'https://10.1.2.105/',
-        },
+  dir: '.',
+  arch: 'x64',
+  asar: true,
+  derefSymlinks: false,
+  download: {
+    cacheRoot: './zips',
+    mirrorOptions: {
+      mirror: 'https://10.1.2.105/',
     },
-    extraResource: ['foo.js', 'bar.js'],
-    ignore: [/ab+c/, new RegExp('abc')],
-    overwrite: false,
-    platform: 'darwin',
-    prune: false,
-    quiet: false,
-    tmpdir: 'false',
-    appBundleId: '123456',
-    appCategoryType: 'public.app-category.developer-tools',
-    extendInfo: 'plist.txt',
-    helperBundleId: '23223f',
-    osxSign: true,
-})
-    .then(onCompleted)
-    .catch(onError);
+  },
+  extraResource: ['foo.js', 'bar.js'],
+  ignore: [/ab+c/, new RegExp('abc')],
+  overwrite: false,
+  platform: 'darwin',
+  prune: false,
+  quiet: false,
+  tmpdir: 'false',
+  appBundleId: '123456',
+  appCategoryType: 'public.app-category.developer-tools',
+  extendInfo: 'plist.txt',
+  helperBundleId: '23223f',
+  osxSign: true,
+}).then(onCompleted).catch(onError);
 
 packager({
-    dir: '.',
-    arch: 'armv7l',
-    asar: {
-        ordering: 'order.txt',
-        unpack: '*.js',
-        unpackDir: 'sub_dir',
+  dir: '.',
+  arch: 'armv7l',
+  asar: {
+    ordering: 'order.txt',
+    unpack: '*.js',
+    unpackDir: 'sub_dir',
+  },
+  download: {
+    cacheRoot: './zips',
+    mirrorOptions: {
+      mirror: 'https://10.1.2.105/',
     },
-    download: {
-        cacheRoot: './zips',
-        mirrorOptions: {
-            mirror: 'https://10.1.2.105/',
-        },
-    },
-    ignore: ignoreFunction,
-    platform: 'linux',
-})
-    .then(onCompleted)
-    .catch(onError);
+  },
+  ignore: ignoreFunction,
+  platform: 'linux',
+}).then(onCompleted).catch(onError);
 
 packager({
-    dir: '.',
-    arch: 'mips64el',
-    electronVersion: '1.8.8',
-    prebuiltAsar: 'prebuilt.asar',
-    platform: 'linux',
-})
-    .then(onCompleted)
-    .catch(onError);
+  dir: '.',
+  arch: 'mips64el',
+  electronVersion: '1.8.8',
+  prebuiltAsar: 'prebuilt.asar',
+  platform: 'linux',
+}).then(onCompleted).catch(onError);
 
 packager({
-    dir: '.',
-    arch: ['ia32', 'x64'],
-    download: {
-        cacheRoot: './zips',
-        mirrorOptions: {
-            mirror: 'https://10.1.2.105/',
-        },
+  dir: '.',
+  arch: ['ia32', 'x64'],
+  download: {
+    cacheRoot: './zips',
+    mirrorOptions: {
+      mirror: 'https://10.1.2.105/',
     },
-    platform: 'mas',
-    extendInfo: {
-        foo: 'bar',
+  },
+  platform: 'mas',
+  extendInfo: {
+    foo: 'bar',
+  },
+  osxNotarize: {
+    appleId: 'My ID',
+    appleIdPassword: 'Bad Password',
+  } as packager.ElectronNotarizeOptions,
+  osxSign: {
+    identity: 'myidentity',
+    entitlements: 'path/to/my.entitlements',
+    'entitlements-inherit': 'path/to/inherit.entitlements',
+  },
+  protocols: [
+    {
+      name: 'myappproto',
+      schemes: ['myapp', 'myapp2'],
     },
-    osxNotarize: {
-        appleId: 'My ID',
-        appleIdPassword: 'Bad Password',
-    } as packager.ElectronNotarizeOptions,
-    osxSign: {
-        identity: 'myidentity',
-        entitlements: 'path/to/my.entitlements',
-        'entitlements-inherit': 'path/to/inherit.entitlements',
-    },
-    protocols: [
-        {
-            name: 'myappproto',
-            schemes: ['myapp', 'myapp2'],
-        },
-    ],
-})
-    .then(onCompleted)
-    .catch(onError);
+  ],
+}).then(onCompleted).catch(onError);
